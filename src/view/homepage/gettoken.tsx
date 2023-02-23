65289202fe25759ec2fb7803429bf0700445d21e
howard <howardwang8998@gmail.com>
2023-02-23 22:24:21 +0800

    0

--- src/view/homepage/gettoken.tsx
+++ src/view/homepage/gettoken.tsx
@@ -1,32 +1,30 @@
-import axios from "axios"
+import axios from 'axios'
 async function getAccessToken(
-    clientId: string,
-    clientSecret: string,
-    code: string,
-  ): Promise<string | undefined> {
-    try {
-      const response = await axios.post(
-        'http://github.com/login/oauth/access_token',
-        
-        {
-          client_id: clientId,
-          client_secret: clientSecret,
-          code: code,
+  clientId: string,
+  clientSecret: string,
+  code: string,
+): Promise<string | undefined> {
+  try {
+    const response = await axios.post(
+      'http://github.com/login/oauth/access_token',
+
+      {
+        client_id: clientId,
+        client_secret: clientSecret,
+        code: code,
+      },
+      {
+        headers: {
+          Accept: 'application/json',
         },
-        {
-          withCredentials: true,
-          headers: {
-            Accept: 'application/json',
-            
-          },
-        },
-      )
-  
-      return response.data.access_token
-    } catch (error) {
-      console.error(error)
-      return undefined
-    }
+      },
+    )
+
+    return response.data.access_token
+  } catch (error) {
+    console.error(error)
+    return undefined
   }
-  
-export default getAccessToken;
+}
+
+export default getAccessToken
