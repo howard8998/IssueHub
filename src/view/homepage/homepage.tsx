5852e45cc8f53d0ac18a7b9e373957c7c94570f4
howard <howardwang8998@gmail.com>
2023-02-24 20:19:54 +0800

    小修小改

--- src/view/homepage/homepage.tsx
+++ src/view/homepage/homepage.tsx
@@ -5,13 +5,13 @@ const homepage = () => {
   gettoken();
   return (
     <Card sx={{mt:8,width:500,mx:'auto',boxShadow:4}}>
-      
-    </Card>
-    <div>
+ <div>
       <div>login succes!</div>
       <button onClick={getdata}>getuser</button>
       <button onClick={logout}>logout</button>
     </div>
+    </Card>
+   
   )
 }
 export default homepage
