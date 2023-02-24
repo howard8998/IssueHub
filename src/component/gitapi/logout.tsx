5bba438819c7b093987a4dc4b74b970e4fe1e1a0
howard <howardwang8998@gmail.com>
2023-02-24 19:36:44 +0800

    小修改

--- src/component/gitapi/logout.tsx
+++ src/component/gitapi/logout.tsx
@@ -1,15 +1,8 @@
 const logout = async () => {
   // 從網址中刪除 code
-  window.location.search = ''
   await sessionStorage.removeItem('accessToken')
-  // 或者，如果要在網址中保留其他 query parameter，可以使用 URLSearchParams 來刪除 code
-  /*const urlParams = new URLSearchParams(window.location.search)
-  urlParams.delete('code')
-  window.history.replaceState(
-    {},
-    '',
-    `${window.location.pathname}?${urlParams}`,
-  )*/
+  window.location.search = ''
+  
 }
 
 export default logout
