889267fd2a97216f54baceb609e925a67c822ce7
howard <howardwang8998@gmail.com>
2023-02-24 20:19:57 +0800

    初版

--- src/view/homepage/homepage.tsx
+++ src/view/homepage/homepage.tsx
@@ -1,17 +1,17 @@
-import getdata from "../../component/gitapi/getdata"
-import logout from "../../component/gitapi/logout"
-import gettoken from "../../component/gitapi/gettoken"
+import getdata from '../../component/gitapi/getdata'
+import logout from '../../component/gitapi/logout'
+import gettoken from '../../component/gitapi/gettoken'
+import { Card } from '@mui/material'
 const homepage = () => {
-  gettoken();
+  gettoken()
   return (
-    <Card sx={{mt:8,width:500,mx:'auto',boxShadow:4}}>
- <div>
-      <div>login succes!</div>
-      <button onClick={getdata}>getuser</button>
-      <button onClick={logout}>logout</button>
-    </div>
+    <Card sx={{ mt: 8, width: 500, mx: 'auto', boxShadow: 4 }}>
+      <div>
+        <div>login succes!</div>
+        <button onClick={getdata}>getuser</button>
+        <button onClick={logout}>logout</button>
+      </div>
     </Card>
-   
   )
 }
 export default homepage
