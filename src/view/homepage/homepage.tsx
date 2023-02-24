caa53c08086a66fb287abf9cbc05678cc04209c8
howard <howardwang8998@gmail.com>
2023-02-24 20:16:57 +0800

    刪除不必要的案

--- src/view/homepage/homepage.tsx
+++ src/view/homepage/homepage.tsx
@@ -4,6 +4,9 @@ import gettoken from "../../component/gitapi/gettoken"
 const homepage = () => {
   gettoken();
   return (
+    <Card sx={{mt:8,width:500,mx:'auto',boxShadow:4}}>
+      
+    </Card>
     <div>
       <div>login succes!</div>
       <button onClick={getdata}>getuser</button>
